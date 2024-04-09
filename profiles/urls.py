f0bfe9a880997f0c43de9f194b0b506031f9ac1e
Gaiden92 <samy_fouchal@hotmail.fr>
2024-04-09 23:57:54 +0200

    Modification url, vues et templates.

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 
-from . import views
 from profiles import views as profiles_views
 
 
 urlpatterns = [
-    path('profiles/', profiles_views.profiles_index, name='profiles_index'),
+    path('profiles/', profiles_views.index, name='profiles_index'),
     path('profiles/<str:username>/', profiles_views.profile, name='profile'),
 ]
