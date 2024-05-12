3735020f65064c417e0c8269af8e6f3ec7206f96
Gaiden92 <samy_fouchal@hotmail.fr>
2024-05-13 00:12:21 +0200

    test deploy

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 set -0 errexit
 exec pip install -r requirements.txt
 exec python ./manage.py collectstatic --no-input
+exec python ./manage.py makemigrations
 exec python ./manage.py migrate
-exec ./manage.py runserver "0.0.0.0:8000"
