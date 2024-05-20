f95821da4e4deefda1c1991c3eaf30162161c41e
Gaiden92 <samy_fouchal@hotmail.fr>
2024-05-20 19:08:09 +0200

    test deploy

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -50,9 +50,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DJANGO_DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1',
+                 'localhost',
+                 'one0cf13e7-latest.onrender.com']
 
 
 # Application definition
