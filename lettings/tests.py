d492e35ef7d3e4eca58d3b0ba241d7d67d270228
Gaiden92 <samy_fouchal@hotmail.fr>
2024-05-26 17:39:45 +0200

    Increase test coverage

--- lettings/tests.py
+++ lettings/tests.py
@@ -1,126 +1,84 @@
 from django.test import TestCase, Client
+from django.urls import reverse
+from django.apps import apps
 
 from .models import Letting, Address
+from lettings.apps import LettingsConfig
 
 
 class TestLetting(TestCase):
-    """A class to represent a lettign test
+    """A class to represent tests for Letting model"""
 
-    Arguments:
-        TestCase -- class: TestCase class
-    """
     def setUp(self) -> None:
-        """Set up configuration for the test
-        """
+        """Method to set up the test configuration"""
         self.address = Address.objects.create(
-            number=9,
-            street="allée des citronniers",
-            city="Suresnes",
-            state="92",
-            zip_code=92150,
-            country_iso_code="FRA")
-        self.letting = Letting.objects.create(title="title letting", address=self.address)
+            number=123,
+            street="Test Street",
+            city="Test City",
+            state="TS",
+            zip_code="12345",
+            country_iso_code="TST",
+        )
+        self.letting = Letting.objects.create(title="Test Letting", address=self.address)
 
-    def test_letting(self):
+    def test_letting(self) -> None:
+        """Method to test letting"""
+        self.assertEqual(self.letting.title, "Test Letting")
         self.assertEqual(self.letting.address, self.address)
 
-    def test_letting_title(self):
-        self.title = "Other title"
-        self.assertNotEqual(self.letting.title, self.title)
 
-    def test_letting_address(self):
-        self.address = Address.objects.create(
-            number=10,
-            street="allée des Bananiers",
-            city="Rueil",
-            state="92",
-            zip_code=92150,
-            country_iso_code="FRA")
+class TestLettingsIndexView(TestCase):
+    """A class to represent tests for the lettings index view"""
 
-        self.assertNotEqual(self.letting.address, self.address)
-
-
-# Test index view
-class TestIndexView(TestCase):
-    """A class to represent a test of the view index
-
-    Arguments:
-        TestCase -- class TestCase
-    """
     def setUp(self) -> None:
-        """Method to set up the test configuration
-        """
+        """Method to set up the test configuration"""
         self.client = Client()
-        self.response = self.client.get('/lettings/')
-
-    def test_response(self):
-        """Method to test response
-        """
-        self.assertEqual(self.response.status_code, 200)
-
-    def test_template(self) -> None:
-        """Method to test template
-        """
-        self.assertTemplateUsed(self.response, 'lettings/index.html')
-
-
-class TestLettingsView(TestCase):
-    """A class to represent a test lettings views
-
-    Arguments:
-        TestCase -- class: TestCase
-    """
-    def setUp(self) -> None:
-        """Method to set up the test configuration
-        """
-        self.address = Address.objects.create(
-            number=9,
-            street="allée des citronniers",
-            city="Suresnes",
-            state="92",
-            zip_code=92150,
-            country_iso_code="FRA")
-        self.letting = Letting.objects.create(title="title letting", address=self.address)
-        self.client = Client()
-        self.response = self.client.get('/lettings/')
+        self.response = self.client.get(reverse('lettings_index'))
 
     def test_response(self) -> None:
-        """Method to test response
-        """
+        """Method to test response"""
         self.assertEqual(self.response.status_code, 200)
 
     def test_template(self) -> None:
-        """Method to test template
-        """
+        """Method to test template"""
         self.assertTemplateUsed(self.response, 'lettings/index.html')
 
 
 class TestLettingView(TestCase):
-    """A class to represent a test letting view
+    """A class to represent tests for the letting view"""
 
-    Arguments:
-        TestCase -- class: TestCase
-    """
     def setUp(self) -> None:
-        """Method to set up the test configuration
-        """
-        self.address = Address.objects.create(
-            number=9,
-            street="allée des citronniers",
-            city="Suresnes",
-            state="92",
-            zip_code=92150,
-            country_iso_code="FRA")
-        self.letting = Letting.objects.create(title="title letting", address=self.address)
+        """Method to set up the test configuration"""
         self.client = Client()
-        self.response = self.client.get(f'/lettings/{self.letting.id}/')
+        self.address = Address.objects.create(
+            number=123,
+            street="Test Street",
+            city="Test City",
+            state="TS",
+            zip_code="12345",
+            country_iso_code="TST",
+        )
+        self.letting = Letting.objects.create(title="Test Letting", address=self.address)
 
-    def test_response(self) -> None:
-        """Method to test response
-        """
-        self.assertEqual(self.response.status_code, 200)
+    def test_letting_view(self) -> None:
+        """Method to test letting view"""
+        response = self.client.get(reverse('letting', args=[self.letting.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'lettings/letting.html')
+        self.assertContains(response, self.letting.title)
+        self.assertContains(response, self.letting.address.street)
 
-    def test_template(self) -> None:
-        """Method to test template
-        """
-        self.assertTemplateUsed(self.response, 'lettings/letting.html')
+    def test_letting_view_nonexistent_letting(self) -> None:
+        """Method to test letting view with a nonexistent letting"""
+        response = self.client.get(reverse('letting', args=[999]))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('lettings_index'))
+
+
+class LettingsConfigTest(TestCase):
+    """A class to represent tests for LettingsConfig"""
+
+    def test_app_config(self):
+        """Method to test the lettings app configuration"""
+        self.assertEqual(LettingsConfig.name, 'lettings')
+        self.assertEqual(apps.get_app_config('lettings').name, 'lettings')
--- profiles/tests.py
+++ profiles/tests.py
@@ -1,134 +1,148 @@
-from django.test import TestCase
-from django.test import Client
+from django.test import TestCase, Client
+from django.urls import reverse
 
 from .models import Profile, User
+from profiles.apps import ProfilesConfig
 
 
 class TestProfile(TestCase):
-    """A class to represent a test profile
-
-    Arguments:
-        TestCase -- object: TestCase
-    """
-    def setUp(self) -> None:
-        """Method to set up the test configuration
-        """
+    """Class for testing the Profile model."""
+    def setUp(self):
+        """Initialization of data for tests."""
         self.data = {
-                "username": "Test",
-                "password": "hello",
-                "email": "test@hotmail.fr",
-                "first_name": "Test Firstname",
-                "last_name": "Test Lastname",
+            "username": "Test",
+            "password": "hello",
+            "email": "test@hotmail.fr",
+            "first_name": "Test Firstname",
+            "last_name": "Test Lastname",
         }
         self.favorite_city = "Suresnes"
         self.user = User.objects.create_user(**self.data)
         self.profile = Profile.objects.create(
             user=self.user,
-            favorite_city=self.favorite_city)
+            favorite_city=self.favorite_city
+        )
 
-    def test_profile(self) -> None:
-        """Method to test profile
-        """
+    def test_profile(self):
+        """Method to test the association between profile and user."""
         self.assertEqual(self.profile.user, self.user)
 
-    def test_profile_favorite_city(self) -> None:
-        """Method to test profile favorite city
-        """
-        self.favorite_city = "Madrid"
-        self.assertNotEqual(self.profile.favorite_city, self.favorite_city)
+    def test_profile_favorite_city(self):
+        """Method to test favorite city in the profile."""
+        self.assertNotEqual(self.profile.favorite_city, "Madrid")
 
 
-# Test index view
 class TestIndexView(TestCase):
-    """A class to represent a test index view
-        Arguments:
-            TestCase -- object: TestCase
-    """
-    def setUp(self) -> None:
-        """Method to set up the test configuration
-        """
+    """Class for testing the index view."""
+    def setUp(self):
+        """Initialization of data for tests."""
         self.client = Client()
         self.profiles_list = Profile.objects.all()
         self.response = self.client.get('/')
 
-    def test_response(self) -> None:
-        """Method to test response
-        """
+    def test_response(self):
+        """Method to test the response of the view."""
         self.assertEqual(self.response.status_code, 200)
 
-    def test_template(self) -> None:
-        """Method to test template
-        """
+    def test_template(self):
+        """Method to test the template used."""
         self.assertTemplateUsed(self.response, 'index.html')
 
 
-# Test index view
 class TestProfileView(TestCase):
-    """A class to represent a test profile view
-
-    Arguments:
-        TestCase -- object: TestCase
-    """
-    def setUp(self) -> None:
-        """Method to set up the test configuration
-        """
+    """Class for testing the profile view."""
+    def setUp(self):
+        """Initialization of data for tests."""
         self.data = {
-                "username": "Test",
-                "password": "hello",
-                "email": "test@hotmail.fr",
-                "first_name": "Test Firstname",
-                "last_name": "Test Lastname",
+            "username": "Test",
+            "password": "hello",
+            "email": "test@hotmail.fr",
+            "first_name": "Test Firstname",
+            "last_name": "Test Lastname",
         }
         self.user = User.objects.create_user(**self.data)
-
         self.profile = Profile.objects.create(
             user=self.user,
-            favorite_city="Rueil")
+            favorite_city="Rueil"
+        )
         self.client = Client()
         self.response = self.client.get(f'/profiles/{self.user.username}/')
 
-    def test_response(self) -> None:
-        """Method to test response
-        """
+    def test_response(self):
+        """Method to test the response of the view."""
         self.assertEqual(self.response.status_code, 200)
 
-    def test_template(self) -> None:
-        """Method to test template
-        """
+    def test_template(self):
+        """Method to test the template used."""
         self.assertTemplateUsed(self.response, 'profiles/profile.html')
 
 
-# Test index view
 class TestProfilesView(TestCase):
-    """A class to represent a test profiles view
-
-    Arguments:
-        TestCase -- object: TestCase
-    """
-    def setUp(self) -> None:
-        """Method to set up the test configuration
-        """
+    """Class for testing the profiles view."""
+    def setUp(self):
+        """Initialization of data for tests."""
         self.data = {
-                "username": "Test",
-                "password": "hello",
-                "email": "test@hotmail.fr",
-                "first_name": "Test Firstname",
-                "last_name": "Test Lastname",
+            "username": "Test",
+            "password": "hello",
+            "email": "test@hotmail.fr",
+            "first_name": "Test Firstname",
+            "last_name": "Test Lastname",
         }
         self.user = User.objects.create_user(**self.data)
-
         self.profile = Profile.objects.create(
             user=self.user,
-            favorite_city="Rueil")
+            favorite_city="Rueil"
+        )
         self.client = Client()
         self.response = self.client.get(f'/profiles/')
 
-    def test_response(self) -> None:
-        """Method to test response
-        """
+    def test_profile_view_nonexistent_user(self):
+        """Method to test redirection for nonexistent user."""
+        response = self.client.get(reverse('profile', args=['nonexistentuser']))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('profiles_index'))
+
+    def test_profile_view(self):
+        """Method to test profile view."""
+        response = self.client.get(reverse('profile', args=['Test']))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'profiles/profile.html')
+
+    def test_response(self):
+        """Method to test the response of the view."""
         self.assertEqual(self.response.status_code, 200)
 
-    def test_template(self) -> None:
-        """Method to test template
-        """
+    def test_template(self):
+        """Method to test the template used."""
         self.assertTemplateUsed(self.response, 'profiles/index.html')
+
+
+class ProfilesURLTests(TestCase):
+    """Class for testing profile URLs."""
+    def setUp(self):
+        """Initialization of data for tests."""
+        self.client = Client()
+
+    def test_profiles_index_url(self):
+        """Method to test the profiles index URL."""
+        response = self.client.get(reverse('profiles_index'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_profile_url_with_valid_username(self):
+        """Method to test profile URL with valid username."""
+        username = 'testuser'
+        response = self.client.get(reverse('profile', args=[username]))
+        self.assertEqual(response.status_code, 302)
+
+    def test_profile_url_with_invalid_username(self):
+        """Method to test profile URL with invalid username."""
+        username = 9
+        response = self.client.get(reverse('profile', args=[username]))
+        self.assertEqual(response.status_code, 302)
+
+
+class ProfilesConfigTest(TestCase):
+    """Class for testing the profiles application configuration."""
+    def test_app_config(self):
+        """Method to test the configuration of the profiles application."""
+        self.assertEqual(ProfilesConfig.name, 'profiles')
