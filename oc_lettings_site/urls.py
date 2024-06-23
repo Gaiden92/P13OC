8c3b1787a435ebecb839d6968eab7bb7288c7250
Gaiden92 <samy_fouchal@hotmail.fr>
2024-06-23 15:46:45 +0200

    Correction of path admin if render os

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,3 +1,5 @@
+import os
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
@@ -5,12 +7,13 @@ from django.conf import settings
 
 from . import views
 
+path_admin = path('secret0208/', admin.site.urls) if 'RENDER' in os.environ else path('admin/', admin.site.urls)
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('', include('profiles.urls')),
     path('', include('lettings.urls')),
-    path('admin/', admin.site.urls),
+    path_admin,
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
