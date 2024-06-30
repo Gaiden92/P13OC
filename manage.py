683a9c8e862edcc59deddda31a277c4d1e50ea9a
Gaiden92 <samy_fouchal@hotmail.fr>
2024-06-30 19:57:12 +0200

    change home

--- manage.py
+++ manage.py
@@ -1,6 +1,5 @@
 import os
 import sys
-import sentry_sdk
 
 
 def main():
@@ -10,22 +9,17 @@ def main():
     Raises:
         ImportError: Exception: PythonPath no environment variable
     """
-    with sentry_sdk.start_transaction(op="load_env_settings"):
-        try:
-            os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oc_lettings_site.settings')
-        except Exception as e:
-            sentry_sdk.capture_exception(e)
-            print(e)
-    with sentry_sdk.start_transaction(op="execute_from_command_line"):
-        try:
-            from django.core.management import execute_from_command_line
-        except ImportError as exc:
-            raise ImportError(
-                "Couldn't import Django. Are you sure it's installed and "
-                "available on your PYTHONPATH environment variable? Did you "
-                "forget to activate a virtual environment?"
-            ) from exc
-        execute_from_command_line(sys.argv)
+
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oc_lettings_site.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
 
 
 if __name__ == '__main__':
